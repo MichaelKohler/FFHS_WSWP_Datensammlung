b435926ad890bff2a5313f5cf336f95c91a5acab
Michael Kohler <mkohler@picobudget.com>
2015-04-03 19:16:22 +0200

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular
+  .module('ffhsWswpDatensammlungApp', [
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
