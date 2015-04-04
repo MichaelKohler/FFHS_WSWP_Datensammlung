c1706109e651b5097d34deb359840f08ed90eeb0
Michael Kohler <mkohler@picobudget.com>
2015-04-04 17:09:31 +0200

    Show Error <div> only when there actually is an error

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,7 +4,6 @@
 angular.module('ffhsWswpDatensammlungApp')
   .controller('MainCtrl', function ($scope, $localStorage, GitHubCommits) {
     $scope.commits = [];
-    $scope.error = '';
     $scope.$storage = $localStorage;
 
     $scope.getRawData = function () {
